186a38a887bce0a1c717e4ec17f12442224a7792
muhammed-looth <muhammedloothhere@gmail.com>
2025-11-19 23:42:38 +0530

    Add new HTML templates and update server routes for enhanced functionality

--- service/server.py
+++ service/server.py
@@ -1,16 +1,16 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, render_template
 import os
 import time
 import json
 import threading
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
 NOTES_DIR = '../notes'
 
 # This is a mock global variable that could leak data in race
 average_response_time = 0
 
-@app.route('/note/<int:note_id>')
+@app.route('/notes/<int:note_id>')
 def get_note(note_id):
     filename = os.path.join(NOTES_DIR, f'note_{note_id}.json')
     if not os.path.exists(filename):
@@ -41,6 +41,51 @@ def get_note(note_id):
     else:
         # Return full data normally
         return jsonify(data)
+
+
+@app.route('/')
+def homePage():
+    return render_template('index.html') 
+
+@app.route('/maze')
+def mazePage():
+    return render_template('maze.html')
+
+@app.route('/glitch')
+def glitchPage():
+    return render_template('glitch.html')
+
+@app.route('/phantom')
+def phantomPage():
+    return render_template('phantom.html')
+
+@app.route('/cipherlab')
+def cipherLabPage():
+    return render_template('cipherlab.html')
+
+@app.route('/mirror')
+def mirrorPage():
+    return render_template('mirror.html')
+
+@app.route('/noise')
+def noisePage():
+    return render_template('noise.html')
+
+@app.route('/neon')
+def neonPage():
+    return render_template('neon.html')
+
+@app.route('/shadow')
+def shadowPage():
+    return render_template('shadow.html')
+
+@app.route('/puzzle')
+def puzzlePage():
+    return render_template('puzzle.html')
+
+@app.route('/labyrinth')
+def labyrinthPage():
+    return render_template('labyrinth.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
